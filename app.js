002ec54d7a683f8c532f15582443bc4f6351c404
karthikeyan98 <karthikeyan98@gmail.com>
2014-09-27 01:50:03 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var express = require('express');
-var app = express();
 
-app.get('/test',function(req,res){
-	res.send('hello');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var fs = require("fs");
+var mongoose = require('mongoose');
+
+
+// To see whats going on inside
+
+mongoose.connect('mongodb://testuser:testuser@127.0.0.1/heroes');
+var models_path = './models';
+fs.readdirSync(models_path).forEach(function (file) {
+    require(models_path+'/'+file)
 });
 
-app.listen(3000);
+var UserModel = mongoose.model('UserModel');
+
+var gcm = require('./controllers/gcm');
+var tracking = require('./controllers/track');
+
+
+
+var app = express();
+app.get('/initialnotify',gcm.initialNotify);
+app.get('/gcmregistration',gcm.gcmRegistration);
+app.get('/acknowledge',tracking.acknowledge);
+app.get('/updateposition',tracking.getallpositions);
+app.get('/getallpositions',tracking.getallpositions);
+
+http.createServer(app).listen(3000, function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
